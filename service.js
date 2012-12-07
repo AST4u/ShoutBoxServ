2ba347eec63ff5a4579c2b7ac3c75668ede70ccd
nanashiRei <nanashi.rei@gmail.com>
2012-12-07 13:30:54 +0100

    Snapshot: Connect und NickServ vollständig implementiert

--- service.js
+++ service.js
@@ -7,9 +7,10 @@ function Client ( ) {
 
     this.irc = require("irc");
     this.con = null;
-    this.colr = require("colors");
     this.req = require("request");
     this.util = require("underscore");
+
+    require("colors");
 
     /** Stored informations */
     this.info = {};
@@ -17,33 +18,81 @@ function Client ( ) {
     /** Event Handlers */
     this.handlers = {
         Ready : function () {
-            bot.performLogin();
+            bot.conf.clientConfig.channels.forEach(function (channel) {
+               bot.con.join(channel);
+               if (bot.handlers.channelMessage[channel]) {
+                   bot.con.addListener("message" + channel, bot.handlers.channelMessage[channel]);
+               }
+            });
+            bot.con.addListener("message#", bot.handlers.channelMessage.all);
+            bot.perform();
         },
-        authNickServ : function () {
+        nickServAuth : function () {
             if (bot.con.connected && bot.opt.nick == bot.conf.nickname) {
+                console.log("info", "Sending indentify to NickServ ...".green);
                 bot.say("NickServ", "IDENTIFY " + bot.conf.nickserv.password);
+            }
+        },
+        nickServLoggedIn : function () {
+            console.log("info", "Logged in to NickServ, performing ready event ...".green);
+            bot.con.opt.authed = true;
+            bot.handlers.Ready();
+        },
+        channelMessage : {
+            all : function (nick, to, text, message) {
+                if (!bot.handlers.channelMessage[to]) {
+                    console.log("info", "G [%s] <%s> %s".gray, to, nick, text);
+                    //TODO: Gloabl command handlers
+                }
+            },
+            "#ast-4-you" : function (nick, to, text, message) {
+                console.log("info", "C [%s] <%s> %s".gray, to, nick, text);
+                //TODO: Relay to DataBase
             }
         }
     };
 
-    /** Connect the bot to IRC & MySQL */
+    /** Connect / Reconnect the bot to IRC & MySQL */
     this.connect = function () {
+        if (bot.con && bot.con.connected) {
+            console.log("info", "Disconnection previous connection ...".yellow);
+            bot.disconnect("Reconnecting...", bot.createConnection);
+        }
+    };
+
+    this.createConnection = function () {
+        console.log("info", "Connecting to IRC Server %s ...".cyan, bot.conf.server);
+
         bot.con = bot.irc.Client(bot.conf.server, bot.conf.nickname, this.conf.clientConfig);
-        this.con.addListener("motd", function (motd) {
-            bot.handlers.Ready();
-            bot.info.motd = motd;
-        });
-        this.con.addListener("notice", function (nick, to, text, message) {
-            if (bot.conf.nickserv.enabled && to == bot.con.opt.nick && nick == "NickServ") {
-                bot.handlers.authNickServ();
+
+        bot.con.addListener("motd", function (motd) {
+            console.log("info", "Received MOTD from Server".cyan);
+            if (!bot.conf.nickserv.enabled) {
+                bot.handlers.Ready();
+                bot.info.motd = motd;
             }
         });
-        this.con.connect();
+
+        bot.con.addListener("notice", function (nick, to, text, message) {
+            if (bot.conf.nickserv.enabled && to == bot.con.opt.nick && nick == "NickServ") {
+                if (text.search(/identify/) != -1) {
+                    bot.handlers.nickServAuth();
+                }
+                if (text.search(/passwort akzeptiert/) != -1) {
+                    bot.handlers.nickServLoggedIn();
+                }
+            }
+        });
+
+        bot.con.connect();
+        bot.con.opt.authed = false;
     };
 
     /** Tries to login with whatever Service that is configured */
-    this.performLogin = function () {
-
+    this.perform = function () {
+        //TODO: Implement performs
     };
 }
 
+var Relay = new Client();
+Relay.connect();
